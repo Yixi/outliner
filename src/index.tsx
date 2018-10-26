802e97da2f8340474e25e1f78a467b54ee8888e1
Yixi <lyh1112@gmail.com>
2018-10-27 00:16:52 +0800

    base react structure

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const mountNode = document.getElementById('MainApp')
+
+ReactDOM.render(<div>outliner</div>, mountNode)
---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,73 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
+
+const PORT = 4222;
+const HOST = 'http://localhost';
+const URL = `${HOST}:${PORT}`;
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    '../src/index.tsx'
+  ],
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, '../dist'),
+    publicPath: ''
+  },
+  context: path.resolve(__dirname, '../src'),
+  devtool: 'source-map',
+  devServer: {
+    stats: {
+      cached: true,
+      chunkModules: false,
+      colors: true
+    },
+    hot: true,
+    compress: true,
+    contentBase: path.resolve(__dirname, '../src'),
+    port: PORT,
+    publicPath: URL,
+    historyApiFallback: true,
+  },
+  resolve: {
+    alias: {
+      "@root": path.resolve(__dirname, "../src"),
+    },
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {enforce: "pre", test: /\.js$/, loader: "source-map-loader"},
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      tsconfig: path.resolve(__dirname, "../tsconfig.json"),
+      tslint: path.resolve(__dirname, "../tslint.json"),
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "../src/app.html",
+      filename: "index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+
+    // prints more readable module names in the browser console on HMR updates
+    //new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+
+  ],
+}
