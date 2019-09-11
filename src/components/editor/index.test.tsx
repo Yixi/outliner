08e0567796e386337a388ab0cfef598db1f5e713
Yixi <lyh1112@gmail.com>
2019-09-11 12:13:06 +0800

    more test

--- src/components/editor/index.test.tsx
+++ src/components/editor/index.test.tsx
@@ -28,4 +28,39 @@ describe('test Editor component', () => {
     expect(mockFn).toHaveBeenCalledWith('ADD', expect.any(EditorState))
 
   })
+
+  test('should handle tab key', () => {
+    const mockFn = jest.fn()
+    const wrapper = mount(<Editor id="1" onCommandEvent={mockFn}/>)
+    const editElement = wrapper.find(EditorSelector)
+
+    editElement.simulate('keyDown', {
+      keyCode: KEY_CODE.TAB,
+    })
+    expect(mockFn).toHaveBeenCalledWith('INDENT', expect.any(EditorState))
+
+    editElement.simulate('keyDown', {
+      keyCode: KEY_CODE.TAB,
+      shiftKey: true,
+    })
+    expect(mockFn).toHaveBeenCalledWith('OUTDENT', expect.any(EditorState))
+  })
+
+  test('should handle arrow key', () => {
+    const mockFn = jest.fn()
+    const wrapper = mount(<Editor id="1" onCommandEvent={mockFn}/>)
+    const editElement = wrapper.find(EditorSelector)
+
+    editElement.simulate('keyDown', {
+      keyCode: KEY_CODE.UP,
+    })
+    expect(mockFn).toHaveBeenCalledWith('UP', expect.any(EditorState))
+
+    editElement.simulate('keyDown', {
+      keyCode: KEY_CODE.DOWN,
+      shiftKey: true,
+    })
+    expect(mockFn).toHaveBeenCalledWith('DOWN', expect.any(EditorState))
+  })
+
 })
